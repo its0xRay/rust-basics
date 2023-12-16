7f91a9a726c683c96573f28c0cdd71029eccb091
its0xRay <99510382+its0xRay@users.noreply.github.com>
2023-12-16 15:48:05 +0530

    Create transpose.rs

---
+++ transpose.rs
@@ -0,0 +1,21 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    result
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103],
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
+}
