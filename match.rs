21f62d35af5c314006f7689ab684f8c76c69bb08
its0xRay <99510382+its0xRay@users.noreply.github.com>
2023-12-30 23:52:42 +0530

    Update match.rs

--- match.rs
+++ match.rs
@@ -5,4 +5,20 @@ fn main() {
         1 => println!("Matched"),
         _ => println!("No Match"),
     }
+
+    //matching enum
+    enum Color {
+        Red,
+        Green,
+        Blue,
+    }
+
+    let my_color = Color::Green;
+
+    // use of match expression to match against an enum variant
+    match my_color {
+        Color::Red => println!("The color is red"),
+        Color::Green => println!("The color is green"),
+        Color::Blue => println!("The color is blue"),
+    }
 }
